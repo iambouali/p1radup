0e070044a7016a8c7498ba3a2e29bf26c29c7799
Tarek Bouali <contact@tarekbouali.com>
2023-12-13 01:58:35 +0100

    Add soft mode option with -s/--soft-mode flag
    This commit introduces a new optional flag -s/--soft-mode to enable a "soft mode" behavior, allowing users to keep duplicate query parameters in different paths and the same hostname. When this flag is provided, the script will not remove duplicate query parameters, providing a more lenient behavior. This enhances the flexibility of the script to cater to different use cases.

--- paradup.py
+++ paradup.py
@@ -1,7 +1,7 @@
 import argparse
 from urllib.parse import urlparse, parse_qs, urlunparse
 
-def process_urls(input_file, output_file):
+def process_urls(input_file, output_file, soft_mode=False):
     seen_params = {}
     result_urls = []
 
@@ -14,10 +14,11 @@ def process_urls(input_file, output_file):
 
             new_params = {param: value for param, value in query_params.items() if param not in seen_params.get(hostname, {})}
 
-            if hostname not in seen_params:
-                seen_params[hostname] = set(query_params.keys())
-            else:
-                seen_params[hostname].update(new_params.keys())
+            if not soft_mode:
+                if hostname not in seen_params:
+                    seen_params[hostname] = set(query_params.keys())
+                else:
+                    seen_params[hostname].update(new_params.keys())
 
             if new_params:
                 new_query = "&".join([f"{param}={value[0]}" for param, value in new_params.items()])
@@ -32,9 +33,10 @@ def main():
     parser = argparse.ArgumentParser(description='Process URLs and remove duplicate query parameters.')
     parser.add_argument('-i', '--input', help='Input file path', required=True)
     parser.add_argument('-o', '--output', help='Output file path', required=True)
+    parser.add_argument('-s', '--soft-mode', help='Keep duplicates in different paths and same hostname', action='store_true')
     args = parser.parse_args()
 
-    process_urls(args.input, args.output)
+    process_urls(args.input, args.output, soft_mode=args.soft_mode)
 
 if __name__ == '__main__':
     main()
