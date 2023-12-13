42ecaa74ef248da9d9fce4761b86e3fbd0047597
Tarek Bouali <contact@tarekbouali.com>
2023-12-13 20:19:27 +0100

    add ASCII banner and set output flag as optional

--- p1radup.py
+++ p1radup.py
@@ -1,7 +1,21 @@
 import argparse
 from urllib.parse import urlparse, parse_qs, urlunparse
+from termcolor import colored
 
-def process_urls(input_file, output_file, soft_mode=False):
+def print_banner():
+    banner = """
+        _               _             
+  _ __ / |_ __ __ _  __| |_   _ _ __  
+ | '_ \| | '__/ _` |/ _` | | | | '_ \ 
+ | |_) | | | | (_| | (_| | |_| | |_) |
+ | .__/|_|_|  \__,_|\__,_|\__,_| .__/ 
+ |_|                           |_|   
+by @iambouali                                                   
+    """
+
+    print(colored(banner, 'green'))
+
+def process_urls(input_file, output_file=None, soft_mode=False):
     seen_params = {}
     result_urls = []
 
@@ -25,14 +39,20 @@ def process_urls(input_file, output_file, soft_mode=False):
                 new_url = urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, parsed_url.params, new_query, parsed_url.fragment))
                 result_urls.append(new_url)
 
-    with open(output_file, 'w') as file:
+    if output_file:
+        with open(output_file, 'w') as file:
+            for url in result_urls:
+                file.write(url + '\n')
+    else:
         for url in result_urls:
-            file.write(url + '\n')
+            print(url)
 
 def main():
+    print_banner()
+
     parser = argparse.ArgumentParser(description='Process URLs and remove duplicate query parameters.')
     parser.add_argument('-i', '--input', help='Input file path', required=True)
-    parser.add_argument('-o', '--output', help='Output file path', required=True)
+    parser.add_argument('-o', '--output', help='Output file path')
     parser.add_argument('-s', '--soft-mode', help='Enable soft mode to preserve duplicates in different paths and the same hostname', action='store_true')
     args = parser.parse_args()
 
