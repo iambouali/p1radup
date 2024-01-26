52c3e43cb654284646d977532ddbc16a5cfa9e4c
Tarek Bouali <contact@tarekbouali.com>
2024-01-26 01:44:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='p1radup',
-    version='1.0.0',
-    scripts=['p1radup/p1radup.py'],
+    version='1.0.5',
+    packages=find_packages(),
     install_requires=[
         'termcolor',
     ],
+    entry_points={
+        'console_scripts': [
+            'p1radup = p1radup.p1radup',
+        ],
+    },
     author='Tarek Bouali',
     author_email='contact@tarekbouali.com',
     description='This script identifies duplicate query parameters within each URL and retains only the first occurrence of each parameter for a given hostname.',
-    url='https://github.com/iambouali/p1radup',  # Replace with your GitHub repository URL
+    url='https://github.com/iambouali/p1radup',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
