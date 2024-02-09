0728a0dbb93219ff03a6bc9d5074d1e540586899
Tarek Bouali <contact@tarekbouali.com>
2024-02-09 15:28:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='p1radup',
-    version='2.0.2',
+    version='2.0.7',
     packages=find_packages(),
     install_requires=[
         'termcolor',
