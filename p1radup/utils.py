ff5367a51e7e6a7269ccb043b95401ce0aa4a20f
Alejandro Aznar <a.aznarcortes@gmail.com>
2024-02-25 03:36:01 +0100

    Improve bad char handling

--- p1radup/utils.py
+++ p1radup/utils.py
@@ -1,6 +1,7 @@
+import validators
 import unicodedata
 
-def is_free_of_control_characters(text):
+def _is_free_of_control_characters(text):
     """
     Check if the given string is free of Unicode control characters.
 
@@ -16,3 +17,29 @@ def is_free_of_control_characters(text):
     - bool: True if the string contains no Unicode control characters, False otherwise.
     """
     return all(unicodedata.category(c) != 'Cc' for c in text)
+
+
+def is_url_valid(new_url):
+    """
+    Validates the given URL by checking for Unicode control characters and validating its format.
+
+    This function first checks if the input string is free of Unicode control characters using the unicodedata.category method. 
+    If it contains control characters, the function prints a message indicating that binary data is being ignored, 
+    and returns False. If no control characters are found, it proceeds to check if the string is a valid URL using 
+    the validators.url function. If the URL is not valid, it prints a message indicating that an invalid URL is being ignored, 
+    and returns False. The function returns True only if the input passes both checks.
+
+    Parameters:
+    - new_url (str): The string to be validated as a URL.
+
+    Returns:
+    - bool: True if the input is a valid URL and free of Unicode control characters, False otherwise.
+    """
+
+    if not _is_free_of_control_characters(new_url):
+        print(f"Ignoring binary data: {new_url}")
+        return False
+    if not validators.url(new_url):
+        print(f"Ignoring invalid URL: {new_url}")
+        return False
+    return True
